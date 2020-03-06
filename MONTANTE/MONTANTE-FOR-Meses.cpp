2a1ffb0e98cc55e175306181312ddb7de56c5ce3
LeticiaMBoldo <leticiamboldo14@gmail.com>
2020-03-06 09:37:30 -0300

    Exercicio de Montante com laço de repetição
    aula de Estrutura de dados com professor Gilmar na fatec de jahu

---
+++ MONTANTE/MONTANTE-FOR-Meses.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+float Montana = 0;
+
+float MONTANA(float taxa, float capital, float n){
+	
+	Montana = capital*pow((1+taxa/100),n);
+	
+	return(Montana);
+	
+}
+
+
+int main(){
+	float taxaJuros, capital, n, Resultado;
+
+	cout<<"Informe a taxa mensal de juros: ";cin>>taxaJuros;
+	cout<<"Informe o capital(Valor Aplicado): ";cin>>capital;
+	n = 1;
+	
+	while (n <= 24){
+		
+	Resultado = MONTANA(taxaJuros, capital, n);
+	
+	cout<<"O valor da montante é de = "<<Resultado<<endl;
+	
+	n++;
+	}
+}
